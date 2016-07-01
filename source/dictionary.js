d4eadfb4c8c298dfdf733e47e2c8f6355acec6c4
patryk.gruszka <patryk.gruszka@oxido.pl>
2016-07-01 13:13:35 +0200

    JSDoc comments

--- source/dictionary.js
+++ source/dictionary.js
@@ -1,18 +1,34 @@
 (function (window, document) {
+    /**
+     * Dictionary Class
+     * @constructor
+     */
     var Dictionary = function() {
         this.reset();
     };
 
+    /**
+     * Sets a dictionary object
+     * @param {Object} dictionary
+     */
     Dictionary.prototype.set = function(dictionary) {
         if (dictionary && (typeof dictionary == 'object')) {
             this.base = dictionary;
         }
     };
 
+    /**
+     * Returns current dictionary
+     * @returns {Object}
+     */
     Dictionary.prototype.get = function() {
         return this.base;
     };
 
+    /**
+     * Extends current dictionary
+     * @param {Object} words
+     */
     Dictionary.prototype.extend = function(words) {
         var base = this.base;
         for (var i in words) {
@@ -22,10 +38,21 @@
         }
     };
 
+    /**
+     * Clear current dictionary
+     */
     Dictionary.prototype.reset = function() {
         this.set({})
     };
 
+    /**
+     * Simple templating function
+     * e.g. template('More than {0}, less than {1}', [5, 10])
+     * should return 'More than 5, less than 10'
+     * @param {String} source
+     * @param {Array} params
+     * @returns {String}
+     */
     Dictionary.prototype.template = function(source, params) {
         if (params === undefined) {
             return source;
@@ -39,6 +66,12 @@
         return source;
     };
 
+    /**
+     * Main translate method
+     * @param {String} source
+     * @param {Array} params
+     * @returns {String}
+     */
     Dictionary.prototype.translate = function(source, params) {
         var translated = this.base[source] || source;
         return this.template(translated, params);
